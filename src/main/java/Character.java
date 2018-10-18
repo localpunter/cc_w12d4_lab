1ce14885a1f162d6967b484b46554761a141e382
nimbus117 <mail@nimbus117.co.uk>
2018-10-18 11:46:32 +0100

    Initial commit

---
+++ src/main/java/Character.java
@@ -0,0 +1,25 @@
+public abstract class Character {
+    private int hp;
+    private int gold;
+
+    public Character(int hp, int gold) {
+        this.hp = hp;
+        this.gold = gold;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+}
