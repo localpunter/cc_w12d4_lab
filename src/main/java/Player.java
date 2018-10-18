50733e8a2eeb93a2c4e83940b8e6e80ce5067ce8
nimbus117 <mail@nimbus117.co.uk>
2018-10-18 11:52:40 +0100

    Added player class and playertype enum

---
+++ src/main/java/Player.java
@@ -0,0 +1,13 @@
+public abstract class Player extends Character {
+
+    private PlayerType type;
+
+    public Player(int hp, int gold, PlayerType type) {
+        super(hp, gold);
+        this.type = type;
+    }
+
+    public PlayerType getType() {
+        return type;
+    }
+}
---
+++ src/main/java/PlayerType.java
@@ -0,0 +1,24 @@
+public enum PlayerType {
+    BARBARIAN(90, 10),
+    DWARF(50, 50),
+    KNIGHT(75, 25),
+    WIZARD(50, 50),
+    WARLOCK(50, 50),
+    CLERIC(50, 50);
+
+    private final int hp;
+    private final int gold;
+
+    PlayerType(int hp, int gold) {
+        this.hp = hp;
+        this.gold = gold;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+}
